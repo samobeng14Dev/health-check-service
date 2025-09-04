db2fab3e814487af40e36736aabd15791619af60
Samuel Obeng <obengsamuel270@gmail.com>
2025-09-04 13:00:23 +0000

    fixed import bug in apps.py, installed drf-yasg

--- health_check/apps.py
+++ health_check/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HeathCheckConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'heath_check'
+    name = 'health_check'
---
+++ health_check/services.py
@@ -0,0 +1,4 @@
+import requests
+import time
+from django.utils import timezone
+from health_check.models import Endpoint, HealthCheckResult
